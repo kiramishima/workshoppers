5619d8494365564981508352f1f5be2a78bc95d1
Paul Arizpe <paul.arizpe@arteika>
2016-03-09 14:16:15 -0600

    End of Learnyounode

--- learnyounode/http_json_api.js
+++ learnyounode/http_json_api.js
@@ -3,12 +3,21 @@ var http = require('http')
 var server = http.createServer(function (req, res) {
   if (req.method !== 'GET') return res.end('send me a GET\n')
 
-  var url = require('url').parse('/test?q=1', true)
-  if(url.pathname === '/api/parsetime') {
-      
+  var url = require('url').parse(req.url, true)
+  var dt = new Date(url.query.iso)
+  var result
+  if (/^\/api\/parsetime/.test(req.url)) {
+    result = {hour: dt.getHours(), minute: dt.getMinutes(), second: dt.getSeconds()}
+  } else if (/^\/api\/unixtime/.test(req.url)) {
+    result = {unixtime: dt.getTime()}
   }
-  if(url.pathname === '/api/unixtime') {
-      
+
+  if (result) {
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify(result))
+  } else {
+    res.writeHead(404)
+    res.end()
   }
 })
 
