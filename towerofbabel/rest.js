4dc547204542df3dd625ae8c8e02265fd6edecfe
Paul Arizpe <paul.arizpe@arteika>
2016-03-11 10:47:17 -0600

    End of Tower of Babel

--- towerofbabel/rest.js
+++ towerofbabel/rest.js
@@ -1,18 +1,35 @@
+// ma solution
 var rawArgs = process.argv.slice(2)
-var args = [];
+var args = []
 
-rawArgs.forEach(val => {
-  let commaSep = val.split(',');
-  commaSep.forEach(val => {
-    if(val !== '') args.push(+val);
-  });
-});
-
-function avg(...args) {
-  let total = args.map((item) => item).reduce((item) => item += item )
+function avg (...args) {
+  rawArgs.forEach(val => {
+    let commaSep = val.split(',')
+    commaSep.forEach(val => {
+        if (val !== '') args.push(+val)
+    })
+  })
+  return args.reduce((prev, curr) => prev += curr) / args.length
 }
 
 // A continuación escribe una función llamada `avg` que calcule el promedio.
 
 
-console.log(avg(...args));
+console.log(avg(...args))
+
+// solution officiel
+var rawArgs = process.argv.slice(2)
+var args = []
+
+rawArgs.forEach(val => {
+  let commaSep = val.split(',')
+  commaSep.forEach(val => {
+    if(val !== '') args.push(+val)
+  })
+})
+
+function avg(...args){
+  return args.reduce((a, b)=>a+b)/args.length
+}
+
+console.log(avg(...args))
